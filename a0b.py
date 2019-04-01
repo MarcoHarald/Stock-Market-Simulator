Fixed moving averages

a0b.py
@@ -24,30 +24,8 @@ def average(array):
 
 # determine mu: price Historical, last known price date, how many days back to calculate value
 def findMu(price, today, daysBack, gen):
-
-    # mu = price[gen,today]-price[gen,today-daysBack]
-
-    for day in range(1,daysBack):
-        downside = 0
-        upside = 0
-        countUpside = 0
-        countDownside = 0
-        if price[gen,today]<1.0:
-            print('today price',price[gen,today])
-            print('hist price', price[gen, today - day])
-            print('gen',gen,'today',today,'day',day)
-        localChange = price[gen,today]-price[gen,today-day]
-
-        if localChange > 0:
-            upside = localChange*localChange+upside
-            countUpside +=1
-        elif localChange < 0:
-            downside = localChange*localChange+downside
-            countDownside +=1
-
-        weightedChange = pow(upside/max(countUpside,1),0.5)-pow(downside/max(countDownside,1),0.5)
-        # print('mu change',weightedChange)
-        return weightedChange/price[gen,today-daysBack]
+    mu = price[gen,today]-price[gen,today-daysBack]
+    return mu
 
 
 def movAvg(price, gen, target, bound):
@@ -71,28 +49,28 @@ def findSigma(price, today, daysBack, mu, gen):
 
 
 # number of scenarios produced
-simulations = 100
+simulations = 10
 
 # days into future prediction
-itrtns = 50
+itrtns = 20
 
 # extent of simulation
-future = 1
+future = 10
 
 # prediction start date, database extent backwards
-today = 100
-daysBack = 50
+today = 20
+daysBack = 10
 
 # set bound for moving average operation
 avgBound = 10
 # set margin for graphing grouping options
-margin = 0.1
+margin = 0.99
 
 # correction coefficients
 drift0 = 0.1
 sigma0 = 0.001
 
-lowerBound = today-70
+lowerBound = today-daysBack
 upperBound = today + future
 
 
@@ -154,7 +132,11 @@ for gen in range(simulations):
                 drift = drift * findMu(gssPrice, day, daysBack, 1)
                 sigma = sigma * findSigma(gssPrice, day, daysBack, drift, 1)
                 #print(localPrice[t])
+                drift = 0.01
+                sigma = 0.0075
+
                 localPrice[t + 1] = localPrice[t] + localPrice[t] * random.normalvariate(drift, sigma)
+                print('sigma',sigma,'drift',drift,'calculated price',localPrice[t+1])
                 #print('check 3')
         gssPrice[gen, day + itrtns] = localPrice[itrtns-1]
        # print('diff:',gssPrice[gen, day + itrtns]/localPrice[0])
@@ -177,19 +159,21 @@ tier2 = []
 averagePrediction = average(lastPrice)
 
 for gen in range(simulations):
-    if averagePrediction*(1 + margin) > gssPrice[gen, itrtns + today] > averagePrediction*(1 - margin):
-        tier1 += [gen]
-    elif averagePrediction*(1 + margin) < gssPrice[gen, itrtns + today]:
-        tier0 += [gen]
-    elif averagePrediction*(1 - margin) > gssPrice[gen, itrtns + today]:
-        tier2 += [gen]
+    tier1 += [gen]
+
+#    if averagePrediction*(1 + margin) > gssPrice[gen, itrtns + today] > averagePrediction*(1 - margin):
+#       tier1 += [gen]
+#    elif averagePrediction*(1 + margin) < gssPrice[gen, itrtns + today]:
+#        tier0 += [gen]
+#    elif averagePrediction*(1 - margin) > gssPrice[gen, itrtns + today]:
+#        tier2 += [gen]
 
 print(f'Simulations within {margin} of average prediction {len(tier1)} : {tier1}')
 
 # create log of all prices at certain time into simulation
 cPrice = []
 for gen in range(simulations):
-    cPrice += [gssPrice[gen, itrtns+today-50]]
+    cPrice += [gssPrice[gen, itrtns+today-1]]
 
 # calculate moving average for values in selected streams
 avgPrices = np.zeros((len(tier1), len(realPrices)))
@@ -275,6 +259,8 @@ plt.plot(orderedPrices, cumul)
 plt.ylabel('Fraction Above Given Price', fontsize=10)
 plt.xlabel('Share Price ($)', fontsize=10)
 plt.show()
+
+
 exit()
 # -----------END OF EXECUTION--------------
 
@@ -357,6 +343,33 @@ plt.plot(t, predPrice3, 'y', label='4° Quintile (Predicted Price)')
 for prices in range(len(array)):
     val = val + prices
 val = val / len(array)
+
+# --------------
+
+exit()
+
+# --------------
+
+downside = 0
+upside = 0
+countUpside = 0
+countDownside = 0
+if price[gen, today] < 1.0:
+    print('today price', price[gen, today])
+    print('hist price', price[gen, today - day])
+    print('gen', gen, 'today', today, 'day', day)
+localChange = price[gen, today] - price[gen, today - day]
+
+if localChange > 0:
+    upside = localChange * localChange + upside
+    countUpside += 1
+elif localChange < 0:
+    downside = localChange * localChange + downside
+    countDownside += 1
+
+weightedChange = pow(upside / max(countUpside, 1), 0.5) - pow(downside / max(countDownside, 1), 0.5)
+# print('mu change',weightedChange)
+#return weightedChange / price[gen, today - daysBack]
 
 
 

a5.py
@@ -8,22 +8,23 @@ import matplotlib.pylab as plt
 def movAvg(price, gen, target, bound):
     # find local moving average
     # target : today date / date of evaluation
-    # bound : how many days back 
+    # bound : how many days back
     avgPrice = 0
-    for day in range(target-bound,target-2):
-        avgPrice = price[gen, day]+ avgPrice
-    return avgPrice/(bound-1)
+    for day in range(target - bound, target):
+        avgPrice = price[gen, day] + avgPrice
+    return avgPrice / (bound)
 
-#----------------------
+# ----------------------
 
 # setting variables
 simulations = 10
 lowerBound = 100
-upperBound =  6000
+upperBound = 600
 
-dataMinBound = max(90,lowerBound)
-dataMaxBound = min(len(data),upperBound)
-#----------------------
+dataMinBound = max(90, lowerBound)
+dataMaxBound = upperBound
+
+# ----------------------
 
 csv_file = 'dataBP.csv'
 csv_reader = csv.reader(csv_file, delimiter=',')
@@ -31,17 +32,18 @@ data = [row for row in csv.reader(open(csv_file))]
 
 # initiate separate arrray
 realPrices = np.zeros(len(data))
-gssPrice = np.zeros((simulations,len(realPrices)))
+counterPrices = np.zeros(len(data))
+gssPrice = np.zeros((simulations, len(realPrices)))
 
 # initiate price prediction array
 # save values of closing price [4] to separate array
-for i in range(0,len(data)-1):
-    realPrices[i] = data[i+1][4]
+for i in range(0, len(data) - 1):
+    realPrices[i] = data[i + 1][4]
 
 print('Closing price data saved to separate array.')
-print('Data points:',len(data))
+print('Data points:', len(data))
 
-#---------------------
+# ---------------------
 
 # intial: assign real prices to all simulations
 for gen in range(simulations):
@@ -49,54 +51,64 @@ for gen in range(simulations):
     for i in range(len(realPrices)):
         gssPrice[gen, i] = realPrices[i]
 
-    
-#automate creation of moving avg names
+# automate creation of moving avg names
 
-movAvgValues = [3,7,30,90]
-movAvgTrends = np.zeros((len(movAvgValues),len(realPrices)))
-subGlobT = np.zeros((len(movAvgValues),len(realPrices)))
+movAvgValues = [3, 7, 30, 90]
+movAvgTrends = np.zeros((len(movAvgValues), len(realPrices)))
+subGlobT = np.zeros((len(movAvgValues), len(realPrices)))
 
-counterPrices = []
+print('CHECK 1')
+print(len(movAvgValues))
 
-for movAvgIndex in range(len(movAvgValues),0):    
+for movAvgIndex in range(0,len(movAvgValues)):
     # global trend graph
-    for i in range(movAvgValues[movAvgIndex],dataMaxBound):
+    print('CHECK 2', movAvgIndex)
+
+    for i in range(movAvgValues[movAvgIndex], dataMaxBound-1):
         # determine moving avg : from first column, starting date, how many days back
-        period = movAvgValues[movAvgIndex]  
-        movAvgTrends[movAvgIndex][i] = movAvg(gssPrice, 1, i+period, period)
-        counterPrices[i]=realPrices[i]
-        #FIX check subtraction sequence
- 
-    for i in range(movAvgValues[movAvgIndex],dataMaxBound):
-        subGlobT[movAvgIndex-1][i] = counterPrices[i]-movAvgTrends[movAvgIndex][i]
-        
-        
+        period = movAvgValues[movAvgIndex]
+        movAvgTrends[movAvgIndex][i] = movAvg(gssPrice, 1, i + period, period)
 
-for i in range(movAvgValues[movAvgIndex],dataMaxBound):
-        subGlobT[3][i] = subGlobT[3][i]-subGlobT[3][i]
-    
+        counterPrices[i] = realPrices[i]
 
+    for i in range(movAvgValues[movAvgIndex], dataMaxBound):
+        subGlobT[movAvgIndex][i] = counterPrices[i] - movAvgTrends[movAvgIndex][i]
 
+# subtracting trends
+for i in range(movAvgValues[movAvgIndex], dataMaxBound):
+    subGlobT[3][i] = subGlobT[3][i] - subGlobT[0][i]
 
 # plot
 t = []
-for i in range(lowerBound,upperBound):
+for i in range(lowerBound, upperBound-1):
     t += [i]
+
+yAxis0 = []
+for i in range(lowerBound, upperBound-1):
+    yAxis0 += [realPrices[i]]
 
 # create vertical axis
 yAxis1 = []
-for i in range(lowerBound,upperBound):
-    yAxis1 += [movAvgTrends[3][i]]
+for i in range(lowerBound, upperBound-1):
+    yAxis1 += [movAvgTrends[0][i]]
 
 yAxis2 = []
-for i in range(lowerBound,upperBound):
-    yAxis2 += [realPrices[i]]
+for i in range(lowerBound, upperBound-1):
+    yAxis2 += [movAvgTrends[1][i]]
 
+yAxis3 = []
+for i in range(lowerBound, upperBound-1):
+    yAxis3 += [movAvgTrends[2][i]]
 
-plt.plot(t, yAxis1, 'b', label='Quarterly Trend')
-plt.plot(t, yAxis2, 'g', label='Real Price')
+yAxis4 = []
+for i in range(lowerBound, upperBound-1):
+    yAxis4 += [movAvgTrends[3][i]]
 
-
+plt.plot(t, yAxis0, 'black', label='Real Price')
+plt.plot(t, yAxis4, 'r', label='Quarterly Trend')
+plt.plot(t, yAxis3, 'y', label='Monthly Trend')
+plt.plot(t, yAxis2, 'g', label='Weekly Trend')
+plt.plot(t, yAxis1, 'b', label='Multi-day Trend')
 plt.legend(loc='upper left')
 plt.ylabel('Price ($)', fontsize=10)
 plt.xlabel('Time (days)', fontsize=10)
@@ -105,32 +117,80 @@ plt.show()
 # --------------------
 
 t = []
-for i in range(lowerBound,upperBound):
+for i in range(lowerBound, upperBound-1):
     t += [i]
-
-#original price
-
-yAxis0 = []
-for i in range(lowerBound,upperBound):
-    yAxis0 += [realPrices[i]]
 
 # create vertical axis
 yAxis1 = []
-for i in range(lowerBound,upperBound):
-    yAxis1 += [subGlobT[2][i]]
+for i in range(lowerBound, upperBound-1):
+    yAxis1 += [subGlobT[0][i]]
 
 yAxis2 = []
-for i in range(lowerBound,upperBound):
-    yAxis2 += [movAvgTrends[3][i]]
+for i in range(lowerBound, upperBound-1):
+    yAxis2 += [subGlobT[1][i]]
 
+yAxis3 = []
+for i in range(lowerBound, upperBound-1):
+    yAxis3 += [subGlobT[2][i]]
 
-plt.plot(t, yAxis1, 'r', label='Monthly Trend')
-plt.plot(t, yAxis1, 'b', label='Weekly Trend')
-#plt.plot(t, yAxis0, 'g', label='Real Price')
+yAxis4 = []
+for i in range(lowerBound, upperBound-1):
+    yAxis4 += [subGlobT[3][i]]
+
+plt.plot(t, yAxis4, 'r', label='Quarterly Trend')
+plt.plot(t, yAxis3, 'y', label='Monthly Trend')
+plt.plot(t, yAxis2, 'g', label='Weekly Trend')
+plt.plot(t, yAxis1, 'b', label='Multi-day Trend')
 
 
 plt.legend(loc='upper left')
 plt.ylabel('Price ($)', fontsize=10)
 plt.xlabel('Time (days)', fontsize=10)
 plt.show()
-    
+
+
+
+t = []
+for i in range(lowerBound, upperBound-1):
+    t += [i]
+
+# create vertical axis
+yAxis1 = []
+for i in range(lowerBound, upperBound-1):
+    yAxis1 += [subGlobT[0][i]]
+
+yAxis2 = []
+for i in range(lowerBound, upperBound-1):
+    yAxis2 += [subGlobT[1][i]]
+
+yAxis3 = []
+for i in range(lowerBound, upperBound-1):
+    yAxis3 += [subGlobT[2][i]]
+
+yAxis4 = []
+for i in range(lowerBound, upperBound-1):
+    yAxis4 += [subGlobT[3][i]]
+
+#plt.plot(t, yAxis4, 'r', label='Quarterly Trend')
+#plt.plot(t, yAxis3, 'y', label='Monthly Trend')
+plt.plot(t, yAxis2, 'g', label='Weekly Trend')
+plt.plot(t, yAxis1, 'b', label='Multi-day Trend')
+
+
+plt.legend(loc='upper left')
+plt.ylabel('Price ($)', fontsize=10)
+plt.xlabel('Time (days)', fontsize=10)
+plt.show()
+
+
+
+
+
+
+
+
+
+
+
+
+